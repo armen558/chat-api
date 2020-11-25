b97e102b87b881a060957f6aee019832397eefa0
armen558 <armenpetrosyan665@gmail.com>
2020-11-25 14:48:26 +0400

    testing

--- test/authTest.js
+++ test/authTest.js
@@ -26,8 +26,8 @@ const app = require('../server');
 
     describe('Array', function() {
       describe('#indexOf()', function() {
-        it('should return -1 when the value is not present', function() {
-          assert.equal([1, 2, 3].indexOf(4), -1);
+        it('should return 1 when the value is 2', function() {
+          assert.equal([1, 2, 3].indexOf(2), 1);
         });
       });
     });
